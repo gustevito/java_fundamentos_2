503cdf7ce43c57ba04e68188ef11a2ba4a62abd6
gustavo <editorkuchi@gmail.com>
2024-08-19 10:12:24 -0300

    1º exercicio

---
+++ errosDiversos/ProgramaComErroCompilacao05.java
@@ -0,0 +1,16 @@
+
+
+/**
+ * verificando erros de exceucao e/ou compilacao
+ * 
+ * @author (Giraffa adpatado de Profa. Silvia Moraes) 
+ * @version (teste )
+ */
+
+public class ProgramaComErroCompilacao05
+{
+    public static void main(String args[]){
+        System.out.println("\fShow!! Estás ficando com nisso!");
+    }
+ 
+}
---
+++ errosDiversos/ProgramaComErroCompilacao06.java
@@ -0,0 +1,14 @@
+
+
+/**
+ * verificando erros de exceucao e/ou compilacao
+ * 
+ * @author (Giraffa adpatado de Profa. Silvia Moraes) 
+ * @version (teste )
+ */
+public class ProgramaComErroCompilacao06
+{
+    public static void main (String args[]){
+        System.out.println("\fFantástico!!!! Corrigiste mais um programa! ");
+    }
+}

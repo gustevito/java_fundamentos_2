6bf85f1a01e9d03e0add5f7527456fdde64b6c90
Gustavo <editorkuchi@gmail.com>
2024-11-24 17:50:58 -0300

    inicio do tf

---
+++ Atividades/6.11.24/tf/Menu.java
@@ -0,0 +1,14 @@
+public class Menu{
+    private void perguntaMenu(){
+        System.out.println("\n[1] ........ Criar Conta");
+        System.out.println("[2] ........ Remover Conta");
+        System.out.println("[3] ........ Depositar");
+        System.out.println("[4] ........ Sacar");
+        System.out.println("[5] ........ Tranferencia Entre Contas");
+        System.out.println("[6] ........ Saldo Corrente");
+        System.out.println("[7] ........ Situacao Atual do Banco");
+        System.out.println("[8] ........ Situacao Atual da Conta");
+        System.out.println("[9] ........ Lista de Contas do Banco");
+        System.out.println("[10] ....... Sair");
+    }
+}
